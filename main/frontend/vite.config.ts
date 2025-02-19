14a4c6e0f85f6c1562ee01d5b3056916483dfa5f
Victor Vedenin <master-vva@narod.ru>
2025-02-19 21:40:58 +0300

    change paths to relative (#14)

--- main/frontend/vite.config.ts
+++ main/frontend/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
+    base: './',
     plugins: [vue(), svgLoader(), VueI18nPlugin({})],
     build: {
       assetsDir: '',
