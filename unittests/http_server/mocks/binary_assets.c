64f5612dc6a3957a4b279e17747493e1229781f2
Ilia Skochilov <ilia.skochilov@wirenboard.com>
2025-09-05 15:14:54 +0300

    Ifdefs for archs

--- unittests/http_server/mocks/binary_assets.c
+++ unittests/http_server/mocks/binary_assets.c
@@ -17,26 +17,31 @@ static const uint8_t html_data[] = {
     0xFB, 0x23, 0x54, 0x99
 };
 
-const uint8_t * binary_favicon_webp_gz_start = favicon_data;
-const uint8_t * binary_favicon_webp_gz_end = favicon_data + ARRAY_SIZE(favicon_data);
+#if defined(__x86_64__)
+/* x86_64: define underscore-prefixed symbols (server) */
+    const uint8_t * _binary_favicon_webp_gz_start = favicon_data;
+    const uint8_t * _binary_favicon_webp_gz_end = favicon_data + ARRAY_SIZE(favicon_data);
 
-const uint8_t * binary_index_css_gz_start = css_data;
-const uint8_t * binary_index_css_gz_end = css_data + ARRAY_SIZE(css_data);
+    const uint8_t * _binary_index_css_gz_start = css_data;
+    const uint8_t * _binary_index_css_gz_end = css_data + ARRAY_SIZE(css_data);
 
-const uint8_t * binary_index_js_gz_start = js_data;
-const uint8_t * binary_index_js_gz_end = js_data + ARRAY_SIZE(js_data);
+    const uint8_t * _binary_index_js_gz_start = js_data;
+    const uint8_t * _binary_index_js_gz_end = js_data + ARRAY_SIZE(js_data);
 
-const uint8_t * binary_index_html_gz_start = html_data;
-const uint8_t * binary_index_html_gz_end = html_data + ARRAY_SIZE(html_data);
+    const uint8_t * _binary_index_html_gz_start = html_data;
+    const uint8_t * _binary_index_html_gz_end = html_data + ARRAY_SIZE(html_data);
 
-const uint8_t * _binary_favicon_webp_gz_start = favicon_data;
-const uint8_t * _binary_favicon_webp_gz_end = favicon_data + ARRAY_SIZE(favicon_data);
+#elif defined(__aarch64__) || defined(__arm64__)
+/* arm64: define non-underscored symbols (host Apple Silicon) */
+    const uint8_t * binary_favicon_webp_gz_start = favicon_data;
+    const uint8_t * binary_favicon_webp_gz_end = favicon_data + ARRAY_SIZE(favicon_data);
 
-const uint8_t * _binary_index_css_gz_start = css_data;
-const uint8_t * _binary_index_css_gz_end = css_data + ARRAY_SIZE(css_data);
+    const uint8_t * binary_index_css_gz_start = css_data;
+    const uint8_t * binary_index_css_gz_end = css_data + ARRAY_SIZE(css_data);
 
-const uint8_t * _binary_index_js_gz_start = js_data;
-const uint8_t * _binary_index_js_gz_end = js_data + ARRAY_SIZE(js_data);
+    const uint8_t * binary_index_js_gz_start = js_data;
+    const uint8_t * binary_index_js_gz_end = js_data + ARRAY_SIZE(js_data);
 
-const uint8_t * _binary_index_html_gz_start = html_data;
-const uint8_t * _binary_index_html_gz_end = html_data + ARRAY_SIZE(html_data);
+    const uint8_t * binary_index_html_gz_start = html_data;
+    const uint8_t * binary_index_html_gz_end = html_data + ARRAY_SIZE(html_data);
+#endif
