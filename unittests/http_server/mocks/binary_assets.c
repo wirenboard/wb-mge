c818e3d1dfddc2f2441e094b4f564a238cc3a474
Ilia Skochilov <ilia.skochilov@wirenboard.com>
2025-09-05 15:37:47 +0300

    binary_assets.c

--- unittests/http_server/mocks/binary_assets.c
+++ unittests/http_server/mocks/binary_assets.c
@@ -17,14 +17,14 @@ static const uint8_t html_data[] = {
     0xFB, 0x23, 0x54, 0x99
 };
 
-const uint8_t * binary_favicon_webp_gz_start asm("_binary_favicon_webp_gz_start") = favicon_data;
-const uint8_t * binary_favicon_webp_gz_end asm("_binary_favicon_webp_gz_end") = favicon_data + ARRAY_SIZE(favicon_data);
+const uint8_t * favicon_start asm("_binary_favicon_webp_gz_start") = favicon_data;
+const uint8_t * favicon_end asm("_binary_favicon_webp_gz_end") = favicon_data + ARRAY_SIZE(favicon_data);
 
-const uint8_t * binary_index_css_gz_start asm("_binary_index_css_gz_start") = css_data;
-const uint8_t * binary_index_css_gz_end asm("_binary_index_css_gz_end") = css_data + ARRAY_SIZE(css_data);
+const uint8_t * index_css_start asm("_binary_index_css_gz_start") = css_data;
+const uint8_t * index_css_end asm("_binary_index_css_gz_end") = css_data + ARRAY_SIZE(css_data);
 
-const uint8_t * binary_index_js_gz_start asm("_binary_index_js_gz_start") = js_data;
-const uint8_t * binary_index_js_gz_end asm("_binary_index_js_gz_end") = js_data + ARRAY_SIZE(js_data);
+const uint8_t * index_js_start asm("_binary_index_js_gz_start") = js_data;
+const uint8_t * index_js_end asm("_binary_index_js_gz_end") = js_data + ARRAY_SIZE(js_data);
 
-const uint8_t * binary_index_html_gz_start asm("_binary_index_html_gz_start") = html_data;
-const uint8_t * binary_index_html_gz_end asm("_binary_index_html_gz_end") = html_data + ARRAY_SIZE(html_data);
+const uint8_t * index_html_start asm("_binary_index_html_gz_start") = html_data;
+const uint8_t * index_html_end asm("_binary_index_html_gz_end") = html_data + ARRAY_SIZE(html_data);
