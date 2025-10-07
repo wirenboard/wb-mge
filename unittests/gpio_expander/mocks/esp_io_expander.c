5f81e59a1a37cf33b0f1eb1afd5df0f08c11963e
Ilia Skochilov <ilia.skochilov@wirenboard.com>
2025-10-07 13:36:53 +0300

    Copilot review

--- unittests/gpio_expander/mocks/esp_io_expander.c
+++ unittests/gpio_expander/mocks/esp_io_expander.c
@@ -4,7 +4,6 @@
 esp_err_t mock_esp_io_expander_print_state_return = ESP_OK;
 int mock_esp_io_expander_print_state_called = 0;
 
-
 esp_err_t esp_io_expander_print_state(esp_io_expander_handle_t handle)
 {
     (void)handle;
