103d5e11f789be53ed05ed2989d33b4cb26ae412
Maxim Toropov <mtoropov@yandex.ru>
2025-10-30 13:27:48 +0300

    Fix review issues

--- unittests/http_server/mocks/wb_test.c
+++ unittests/http_server/mocks/wb_test.c
@@ -2,12 +2,12 @@
 
 esp_err_t wb_test_get_handler(httpd_req_t *req)
 {
-    mock_wb_test_get_handler_called = 1;
+    mock_wb_test_get_handler_called++;
     return ESP_OK;
 }
 
 esp_err_t wb_test_post_handler(httpd_req_t *req)
 {
-    mock_wb_test_post_handler_called = 1;
+    mock_wb_test_post_handler_called++;
     return ESP_OK;
 }
